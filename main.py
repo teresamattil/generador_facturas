522539dade9d9f325a1f0642ae95e4a979f94884
Teresa <73397956+teresamattil@users.noreply.github.com>
2026-01-18 12:34:03 +0100

    Adaptaciones vairables 2026

--- main.py
+++ main.py
@@ -19,33 +19,39 @@ if st.button("Generar facturas") and excel_file and docx_template:
     with zipfile.ZipFile(zip_path, "w") as zipf:
         for i, row in df.iterrows():
             doc = DocxTemplate(docx_template)
-            subtotal = float(row["pago_anual"])
+            subtotal = float(row["cuota_anual"])
             iva_pct = float(row["pct_iva"])
             total = subtotal + iva_pct * subtotal
 
             #Extras
             subtotal_extra = float(row("cuota_extra"))
             iva_pct_extra = float(row("pct_iva_extra"))
-            total_extra = subtotal + iva_pct * subtotal
+            total_extra = subtotal_extra + iva_pct_extra * subtotal_extra
+
+            total_final = total + total_extra
 
             context = {
                 "nombre": row["Nombre"],
-                "CIF": row["CIF"],
-                "tipo_cuota": row["tipo_cuota"],
-                "pago_anual": "{:.2f}".format(subtotal).replace('.', ','),
                 "direccion": row["direccion"],
                 "codigo_postal": row["codigo_postal"],
                 "municipio": row["municipio"],
+                "CIF": row["CIF"],
+                "num_factura": f"{datetime.now(UTC).year}{(i + 1):03d}",
+                "fecha": datetime.now(UTC).strftime("%d/%m/%Y"),
+
                 "tipo_socio": row["tipo_socio"],
+                "cuota_anual": "{:.2f}".format(subtotal).replace('.', ','),
+                "pct_iva_socio": "{:.2f}".format(iva_pct).replace('.', ','),
+                "valor_iva_socio": "{:.2f}".format(subtotal * iva_pct).replace('.', ','),
+
                 "tipo_extra": row["tipo_extra"],
                 "cuota_extra": row["cuota_extra"],
                 "pct_iva_extra": row["pct_iva_extra"],
-                "pct_iva": "{:.2f}".format(iva_pct).replace('.', ','),
-                "valor_iva": "{:.2f}".format(subtotal * iva_pct).replace('.', ','),
-                "num_factura": f"{datetime.now(UTC).year}{(i + 1):03d}",
-                "fecha": datetime.now(UTC).strftime("%d/%m/%Y"),
-                "total": "{:.2f}".format(total).replace('.', ','),
                 "valor_iva_extra": "{:.2f}".format(total_extra).replace('.', ','),
+
+
+                "total": "{:.2f}".format(total).replace('.', ','),
+
             }
 
             doc.render(context)
